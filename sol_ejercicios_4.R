aa8abed64bb4d9ed604eebf97163e619e0384458
Gustavo Méndez <gmendezbarbato@gmail.com>
2024-05-15 12:40:48 -0300

    ejercicios_factores

---
+++ sol_ejercicios_4.R
@@ -0,0 +1,16 @@
+##----------------------------------------------------------------------------##
+#                                                                              #
+#    Medición y Diseño 2024 | Carneiro y Méndez                                #
+#    EJERCICIOS CLASE 4. FACTORES                                              #
+#                                                                              #
+##----------------------------------------------------------------------------##
+
+## EJERCICIO FACRTORES ----
+
+# (1) Crear un factor de nombre nivel_edu, que tenga 10 elementos y que sus categorías sean "Alto", "Medio" y "Bajo"
+
+# (2) Crear una tabla de frecuencia con la función fct_count() de forcats
+
+# (3) Crear una tabla de frecuencia con la función fct_count() de forcats, qeu además ordene según frecuencia y calcule la frecuencia relativa (proporción)
+
+# (4) Cambiar los nombres de los niveles por "Más de 12 años", "Entre 6 y 12 años", "Menos de 6 años"). Si tiene dudas utilice foros o la documentación del paquete
